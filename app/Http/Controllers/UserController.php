f0feb8ec5cc6f9414cab9bb3e87fd3862cd038ec
Sajidan03 <rifansyahsajidan03@gmail.com>
2025-09-17 10:40:05 +0700

    sajidan

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exports\UsersExport;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class UserController extends Controller
+{
+    //
+    public function export()
+    {
+        return Excel::download(new UsersExport, 'users.xlsx');
+    }
+}
